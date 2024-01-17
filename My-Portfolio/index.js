91f8177c75693475e570f5712e54fc85a2e7c4e0
Mansi Ravindra Bagul <135009402+mansibagul82@users.noreply.github.com>
2024-01-18 01:23:37 +0530

    Add files via upload

---
+++ My-Portfolio/index.js
@@ -0,0 +1,22 @@
+/* ==================================== typing animation ==================================== */
+
+var typed = new Typed(".typing",{
+    strings: ["Full Stack Developer","Front-End Developer", "Coder"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+});
+
+var typed1 = new Typed(".name",{
+    strings: ["мαηѕι Bagul"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+});
+
+var typed2 = new Typed("#full",{
+    strings: ["👋 Hey I'm Mansi Bagul and"],
+    typeSpeed:80,
+    BackSpeed:60,
+    loop:true
+});
